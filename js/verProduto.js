4b93441d9a3a4f8b7ce6b87c05ee4da7015baff4
thiagofeldner <tfeldner@hotmail.com>
2023-07-26 14:27:40 -0300

    iniciando a listagem por categorias

--- js/verProduto.js
+++ js/verProduto.js
@@ -1,10 +1,47 @@
-const btnVerProduto = document.getElementById('verProduto')
-btnVerProduto.addEventListener('click', verProdutoSelecionado)
 
-function verProdutoSelecionado() {
+const btnVerProduto = document.getElementById('starwars')
+btnVerProduto.addEventListener('click', verCategoriaStarWars)
+
+function verCategoriaStarWars() {
   let produtoSelecionados = produtos.filter(produto => produto.categoria == 'Star Wars')
-  console.table(produtoSelecionados)
+    produtoSelecionados.forEach((produto) => {
+    btnVerProduto.innerHTML += `      
+    <section class="produto__container" id="starwars" data-produtos>
+    <div class="produto__titulo">
+      <h3>${produto.categoria}</h3>
+      <a href="./pages/produtos.html"
+        >Ver tudo
+        <img src="./assets/imagens/seta.svg" alt="" />
+      </a>
+    </div>
+    <ul class="produto__conteudo" >
+      <li>
+        <img src="${produto.imagem}" alt="" />
+        <span>${produto.name}</span>
+        <strong>R$ ${produto.preco}</strong>
+        <a href="#">Ver Produto</a> 
+      </li>
+    </ul>
+  </section>
+    `
+  })
+
+  window.location.href = './pages/produto.html'
 }
 
 
+const btnVerConsoles = document.getElementById('consoles')
+btnVerConsoles.addEventListener('click', verCategoriaConsoles)
 
+function verCategoriaConsoles() {
+  let categoriaSelecionada = produtos.filter(produto => produto.categoria == 'Consoles')
+  console.table(categoriaSelecionada)
+}
+
+const btnVerDiversos = document.getElementById('verDiversos')
+btnVerDiversos.addEventListener('click', verCategoriaDiversos)
+
+function verCategoriaDiversos() {
+  let produtoSelecionados = produtos.filter(produto => produto.categoria == 'Diversos')
+  console.table(produtoSelecionados)
+}
