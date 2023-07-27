2c8b3fa3abe31ed82d48a0a04111fbd3b7e5f4d5
Thiago Feldner <tfeldner@hotmail.com>
2023-07-26 21:49:38 -0300

    incluido funcao verprodutos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let produtos = [];
 const url = "https://64c030c60d8e251fd111fd32.mockapi.io/produtos";
+
 getVerProdutos();
 
 async function getVerProdutos() {
@@ -7,4 +8,5 @@ async function getVerProdutos() {
   produtos = await verProdutos.json();
   
   exibirProdutos(produtos);
+  verProtudosPorCategoria(produtos);
 }
