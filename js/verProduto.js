9d82965ae50e5e65f44e028c9ebacd4bef5617f0
Thiago Feldner <tfeldner@hotmail.com>
2023-07-26 21:50:06 -0300

    criado funcao para listar produto por categoria

--- js/verProduto.js
+++ js/verProduto.js
@@ -1,47 +1,45 @@
+// function verProtudosPorCategoria(produtos){
+//   const produtosCategoria = {
+//     'Star Wars': document.getElementById('starWars'),
+//     'Consoles': document.getElementById('consoles'),
+//     'Diversos': document.getElementById('diversos'),
+//   };
 
-const btnVerProduto = document.getElementById('starwars')
-btnVerProduto.addEventListener('click', verCategoriaStarWars)
+//   produtos.forEach(produto => {
+//     const categoria = produto.categoria
+//     if(produtosCategoria[categoria]) {
+//       produtosCategoria[categoria].innerHTML += `
+//         <li data-id="${produto.id}">
+//           <img src="${produto.imagem}" alt="${produto.alt}" />
+//           <span>${produto.name}</span>
+//           <strong>R$ ${produto.preco}</strong>
+//           <a href="./pages/produto.html?id=${produto.id}&categoria=${produto.categoria}" data-id="${produto.id}">Ver Produto</a>
+//         </li>
+//       `
+//     }
+//   })
+// }
+
+const btnVerStarWars = document.getElementById('starWars')
+btnVerStarWars.addEventListener('click', verCategoriaStarWars)
 
 function verCategoriaStarWars() {
-  let produtoSelecionados = produtos.filter(produto => produto.categoria == 'Star Wars')
-    produtoSelecionados.forEach((produto) => {
-    btnVerProduto.innerHTML += `      
-    <section class="produto__container" id="starwars" data-produtos>
-    <div class="produto__titulo">
-      <h3>${produto.categoria}</h3>
-      <a href="./pages/produtos.html"
-        >Ver tudo
-        <img src="./assets/imagens/seta.svg" alt="" />
-      </a>
-    </div>
-    <ul class="produto__conteudo" >
-      <li>
-        <img src="${produto.imagem}" alt="" />
-        <span>${produto.name}</span>
-        <strong>R$ ${produto.preco}</strong>
-        <a href="#">Ver Produto</a> 
-      </li>
-    </ul>
-  </section>
-    `
-  })
-
-  window.location.href = './pages/produto.html'
+  let categoriaSelecionada = produtos.filter(produto => produto.categoria == 'Star Wars')
+  console.log(categoriaSelecionada)
 }
-
 
 const btnVerConsoles = document.getElementById('consoles')
 btnVerConsoles.addEventListener('click', verCategoriaConsoles)
 
 function verCategoriaConsoles() {
   let categoriaSelecionada = produtos.filter(produto => produto.categoria == 'Consoles')
-  console.table(categoriaSelecionada)
+  console.log(categoriaSelecionada)
 }
 
-const btnVerDiversos = document.getElementById('verDiversos')
+const btnVerDiversos = document.getElementById('diversos')
 btnVerDiversos.addEventListener('click', verCategoriaDiversos)
 
 function verCategoriaDiversos() {
   let produtoSelecionados = produtos.filter(produto => produto.categoria == 'Diversos')
-  console.table(produtoSelecionados)
+  console.log(produtoSelecionados)
 }
