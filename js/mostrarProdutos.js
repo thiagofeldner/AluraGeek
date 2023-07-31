52c606674c0b65eb90b8d2d908832dedd54a3279
thiagofeldner <tfeldner@hotmail.com>
2023-07-31 10:02:39 -0300

    criaçao do arquivo de listagem de produtos

---
+++ js/mostrarProdutos.js
@@ -0,0 +1,34 @@
+import { conectaApi } from "./conectaApi.js";
+
+const lista = document.querySelector("[data-lista]");
+
+function constroiCard(imagem, alt, name, preco, id) {
+  const produto = document.createElement("li");
+  produto.className = "produtos__item";
+  produto.innerHTML = `
+    <li class="produtos__item">
+      <img src="${imagem}" alt="${produto.alt}" />
+      <span>${name}</span>
+      <strong>R$ ${preco}</strong>
+      <span># ${id}</span>
+      <button class="btn__deletar">
+        <img src="../assets/imagens/delete.svg" alt="">
+      </button>
+      <a href="#">
+        <button class="btn__editar">
+          <img src="../assets/imagens/editar.svg" alt="">
+        </button>
+      </a>
+    </li> `;
+
+    return produto;
+}
+
+async function listaProdutos() {
+  const listaApi = await conectaApi.listaProdutos();
+  listaApi.forEach(produto => lista.appendChild(constroiCard(produto.imagem, produto.alt, produto.name, produto.preco, produto.id)));
+
+}
+
+listaProdutos();
+
