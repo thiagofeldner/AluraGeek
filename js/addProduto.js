72b677b42ee028ed270aa830928ce590a485bee3
thiagofeldner <tfeldner@hotmail.com>
2023-08-04 11:21:09 -0300

    reorganizado o código

--- js/addProduto.js
+++ js/addProduto.js
@@ -8,16 +8,10 @@ async function criarProduto(evento) {
   const categoria = document.querySelector("[data-categoria]").value;
   const imagem = document.querySelector("[data-imagem]").value;
   const nome = document.querySelector("[data-nome]").value;
-  const preco = document.querySelector("[data-preco]").value;
+  const preco = document.querySelector("[data-preco]").value.replace("." , ",");
   const descricao = document.querySelector("[data-descricao]").value;
 
-  await conectaApi.addProduto(
-    categoria, 
-    imagem, 
-    nome, 
-    preco, 
-    descricao,
-  );
+  await conectaApi.addProduto(categoria, imagem, nome, preco, descricao);
 
   window.location.href = "../pages/produtos.html";
 }
