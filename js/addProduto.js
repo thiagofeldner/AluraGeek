fd1fa45fd718d5452e4c9d68696dd83cc73a96f2
thiagofeldner <tfeldner@hotmail.com>
2023-08-07 13:51:52 -0300

    alteração na importação da conexão

--- js/addProduto.js
+++ js/addProduto.js
@@ -1,4 +1,4 @@
-import { conectaApi } from "./conectaApi.js";
+import { conectaApi } from "../js/conectaApi.js";
 
 const formulario = document.querySelector("[data-form]");
 
@@ -8,12 +8,12 @@ async function criarProduto(evento) {
   const categoria = document.querySelector("[data-categoria]").value;
   const imagem = document.querySelector("[data-imagem]").value;
   const nome = document.querySelector("[data-nome]").value;
-  const preco = document.querySelector("[data-preco]").value.replace("." , ",");
+  const preco = document.querySelector("[data-preco]").value.replace("," , ".");
   const descricao = document.querySelector("[data-descricao]").value;
 
   await conectaApi.addProduto(categoria, imagem, nome, preco, descricao);
 
-  window.location.href = "../pages/produtos.html";
+  window.location.href = "../index.html";
 }
 
 formulario.addEventListener("sumbit", evento => criarProduto(evento));
